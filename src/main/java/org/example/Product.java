365b4c854ced8b8ac3a7bd7039a7903823c1b18b
Mateusz Gałecki <m.galecki10@gmail.com>
2023-07-07 16:41:52 +0200

    Add equals and hashCode

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
     private String name;
@@ -40,5 +41,24 @@ public class Product {
     @Override
     public String toString() {
         return name + " " + price + " " + currencyCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price) &&
+                Objects.equals(currencyCode, product.currencyCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, currencyCode);
     }
 }
